1e7de155706d6cb98b8e68f19e8002bd26049ebc
elisa darthenay <elisa.darthenay@epfedu.fr>
2022-10-25 12:31:10 +0200

    TP 2 ex2 creation menu fini

--- TP2_convertisseurObjet_DARTHENAY/src/tp2_convertisseurobjet_darthenay/TP2_convertisseurObjet_DARTHENAY.java
+++ TP2_convertisseurObjet_DARTHENAY/src/tp2_convertisseurobjet_darthenay/TP2_convertisseurObjet_DARTHENAY.java
@@ -8,6 +8,7 @@ package tp2_convertisseurobjet_darthenay;
  *
  * @author Elisa
  */
+import java.util.Scanner;
 public class TP2_convertisseurObjet_DARTHENAY {
 
     /**
@@ -22,8 +23,87 @@ public class TP2_convertisseurObjet_DARTHENAY {
     Convertisseur Convertisseur2=new Convertisseur();
     Convertisseur2.FarenheitVersKelvin(25);
     Convertisseur2.KelvinVersFarenheit(87);
-    Convertisseur2.FarenheitversCelsius(46);
+    Convertisseur2.FarenheitVersCelcius(46);
      System.out.println(Convertisseur2);
     
-    }   
-}
+        // création du menu
+
+        Scanner sc = new Scanner(System.in); //creation scanner pour que l'utilisateur puisse saisir les données
+
+        Convertisseur Convertisseur3 = new Convertisseur(); //creation convertisseur 3 pour le menu
+
+              System.out.println("Saisir une valeur : "); //utilisateur saisit une vlr
+
+              double valeur = sc.nextDouble();
+                  // Affichage de chaque cas pour les convertions à choisir        
+              System.out.println("Saisir la convertion à effectuer :");
+
+              System.out.println("1) Celcius vers Kelvin");
+
+              System.out.println("2) Kelvin vers Celcius");
+
+              System.out.println("3) Farenheit vers Celcius");
+
+              System.out.println("4) Celcius vers Farenheit");
+
+              System.out.println("5) Kelvin vers Farenheit");
+
+              System.out.println("6) Farenheit vers Kelvin");
+
+              int convUtilisateur = sc.nextInt();
+
+              double answer;      
+
+              if (convUtilisateur == 1) { //en fonction réponse de conversion
+
+                  answer = Convertisseur3.CelciusVersKelvin(valeur); //converti vlr entrée
+
+                  System.out.println(valeur + " degres Celcius = "+answer+" degres Kelvin"); //renvoi vlr converti à utilisateur
+
+              }
+
+                    
+
+              if (convUtilisateur == 2) { 
+
+                  answer = Convertisseur3.KelvinVersCelcius(valeur);
+
+                  System.out.println(valeur + " degres Kelvin = "+answer+" degres Celcius");
+
+              }
+
+                     
+
+              if (convUtilisateur == 3) {
+
+                  answer = Convertisseur3.FarenheitVersCelcius(valeur);
+
+                  System.out.println(valeur + " degres Farenheit = "+answer+" degres Celcius");
+
+              }
+
+              if (convUtilisateur == 4) {
+
+                  answer = Convertisseur3.CelciusVersFarenheit(valeur);
+
+                  System.out.println(valeur + " degres Celcius = "+answer+" degres Farenheit");
+
+              }
+                     
+              if (convUtilisateur == 5) {
+
+                  answer = Convertisseur3.KelvinVersFarenheit(valeur);
+
+                  System.out.println(valeur + " degres Kelvin = "+answer+" degres Farenheit");
+
+              }
+                 
+              if (convUtilisateur == 6) {
+
+                  answer = Convertisseur3.FarenheitVersKelvin(valeur);
+
+                  System.out.println(valeur + " degres Farenheit = "+answer+" degres Kelvin");
+                  
+              }
+    }
+} 
