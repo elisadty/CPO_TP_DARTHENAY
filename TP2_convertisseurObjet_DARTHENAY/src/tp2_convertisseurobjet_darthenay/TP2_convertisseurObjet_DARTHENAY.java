84e1a7f55651254ce8b219501dc5bba731d74228
elisa darthenay <elisa.darthenay@epfedu.fr>
2022-10-25 11:04:30 +0200

    TP 2 ex2 creation 2ieme exemple

--- TP2_convertisseurObjet_DARTHENAY/src/tp2_convertisseurobjet_darthenay/TP2_convertisseurObjet_DARTHENAY.java
+++ TP2_convertisseurObjet_DARTHENAY/src/tp2_convertisseurobjet_darthenay/TP2_convertisseurObjet_DARTHENAY.java
@@ -18,8 +18,12 @@ public class TP2_convertisseurObjet_DARTHENAY {
     Convertisseur1.CelciusVersFarenheit(600);
     Convertisseur1.CelciusVersKelvin(29);
     System.out.println(Convertisseur1);
-
-
-    }
     
+    Convertisseur Convertisseur2=new Convertisseur();
+    Convertisseur2.FarenheitVersKelvin(25);
+    Convertisseur2.KelvinVersFarenheit(87);
+    Convertisseur2.FarenheitversCelsius(46);
+     System.out.println(Convertisseur2);
+    
+    }   
 }
