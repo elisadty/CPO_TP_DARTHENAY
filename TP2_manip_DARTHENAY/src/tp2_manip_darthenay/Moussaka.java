285e913c9aa9b0f2dfecca92b90f5a7dcdc55371
elisa darthenay <elisa.darthenay@epfedu.fr>
2022-10-25 12:58:57 +0200

    TP 2 ex3 question 5

---
+++ TP2_manip_DARTHENAY/src/tp2_manip_darthenay/Moussaka.java
@@ -0,0 +1,19 @@
+package tp2_manip_darthenay;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Elisa
+ */
+public class Moussaka {
+    int nbCalories ;
+    
+    public Moussaka ( int nbCal) { //initialisation dans concepteur
+        nbCalories = nbCal;
+
+    }
+}
---
+++ TP2_manip_DARTHENAY/src/tp2_manip_darthenay/Tartiflette.java
@@ -0,0 +1,18 @@
+package tp2_manip_darthenay;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Elisa
+ */
+public class Tartiflette {
+    int nbCalories ;
+    
+    public Tartiflette(int nbCal){
+        nbCalories = nbCal;      
+    }
+}
