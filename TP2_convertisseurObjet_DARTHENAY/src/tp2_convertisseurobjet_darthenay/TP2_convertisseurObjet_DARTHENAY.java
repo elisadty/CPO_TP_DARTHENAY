c00ed686897de0bed4d6fe59d3794378a22881b2
elisa darthenay <elisa.darthenay@epfedu.fr>
2022-10-25 11:02:04 +0200

    TP 2 ex2 creation premier exemple

---
+++ TP2_convertisseurObjet_DARTHENAY/src/tp2_convertisseurobjet_darthenay/TP2_convertisseurObjet_DARTHENAY.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_darthenay;
+
+/**
+ *
+ * @author Elisa
+ */
+public class TP2_convertisseurObjet_DARTHENAY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Convertisseur Convertisseur1 = new Convertisseur();
+    Convertisseur1.CelciusVersFarenheit(600);
+    Convertisseur1.CelciusVersKelvin(29);
+    System.out.println(Convertisseur1);
+
+
+    }
+    
+}
