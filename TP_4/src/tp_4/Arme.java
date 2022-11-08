58c41009743015b7b03cecf9d18c7db55fdd32fd
elisa darthenay <elisa.darthenay@epfedu.fr>
2022-11-08 15:10:38 +0100

    début du tp3

---
+++ TP_4/src/tp_4/Arme.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp_4;
+
+/**
+ *
+ * @author Elisa
+ */
+public class Arme {
+  String Nom;
+  int Niveauattaque;
+  
+ public class Baton{
+     
+     
+ }
+ public class Epee{
+     
+ }
+}
---
+++ TP_4/src/tp_4/TP_4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp_4;
+
+/**
+ *
+ * @author Elisa
+ */
+public class TP_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
