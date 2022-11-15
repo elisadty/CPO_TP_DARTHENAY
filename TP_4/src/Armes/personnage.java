ad0de7bdf1c1c0b0fbdd9f776c252ea42ef10cb8
elisa darthenay <elisa.darthenay@epfedu.fr>
2022-11-15 12:37:25 +0100

    tp3) tableau pour les cinq armes

--- TP_4/src/Armes/personnage.java
+++ TP_4/src/Armes/personnage.java
@@ -1,5 +1,7 @@
 package Armes;
 
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,13 +14,21 @@ package Armes;
 public class personnage {
     String nom; 
     int vie;
+    ArrayList inventaire;
+    Arme arm ;
 
     public personnage(String nom, int vie) {
         this.nom = nom;
         this.vie = vie;
+        
+    ArrayList<Arme> inventaire = new ArrayList<Arme> ();
+     
+    if (inventaire.size()<5){
+         inventaire.add(arm);
     }
     
-    
+   
+}
     
     
     
