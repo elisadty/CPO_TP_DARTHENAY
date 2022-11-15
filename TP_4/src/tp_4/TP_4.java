91d7151fc9d6682731bf23de370a0dc876f6ed5d
elisa darthenay <elisa.darthenay@epfedu.fr>
2022-11-15 10:32:59 +0100

    tp3 tableau dynamique crée

--- TP_4/src/tp_4/TP_4.java
+++ TP_4/src/tp_4/TP_4.java
@@ -3,11 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp_4;
+import jav.until.ArryLister;
 
 /**
  *
  * @author Elisa
  */
+import java.util.ArrayList;
 public class TP_4 {
 
     /**
@@ -18,13 +20,24 @@ public class TP_4 {
         
         Epee exca = new Epee("Excalibur",7,5);
         Epee durandal= new Epee("Durandal",4,7); 
-         System.out.println(exca);
-          System.out.println(durandal);
+        System.out.println(exca);
+        System.out.println(durandal);
           
         baton chen = new baton ("Chene",4,5);
         baton chrm = new baton ("Charme",5,6);
-         System.out.println(chen);
-          System.out.println(chrm);
+        System.out.println(chen);
+        System.out.println(chrm);
+        
+        
+        ArrayList<Arme> tabdyn = new ArrayList<Arme>();
+        tabdyn.add(chen);
+        tabdyn.add(chrm); 
+        tabdyn.add(exca);
+        tabdyn.add(durandal);
+        
+        System.out.println(tabdyn);
+        
+      
          
     }
       
